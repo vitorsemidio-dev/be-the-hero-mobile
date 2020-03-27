376fa0003c8a36bfd2ffda33a8bbfa91857f25e4
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 20:26:23 -0300

    Estrutura Header Detalhe

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+import logoImg from '../../assets/logo.png';
 
 export default function Detail() {
   return (
-    <View >
-      <Text>
-        Hello Detalhe
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+
+        <TouchableOpacity onPress={() => {}}>
+          <Feather name="arrow-left" size={28} color="#e02041" />
+          
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
