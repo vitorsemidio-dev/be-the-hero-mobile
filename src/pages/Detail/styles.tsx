7d07e08d556edc5b13d31a410d27caf637344824
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 20:41:48 -0300

    Estilos detalhe

--- src/pages/Detail/styles.tsx
+++ src/pages/Detail/styles.tsx
@@ -1,27 +1,79 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-  
-  container: {},
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
 
-  header: {},
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 
-  incident: {},
+  incident: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+    marginTop: 48,
+  },
 
-  incidentProperty: {},
+  incidentProperty: {
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold',
+    marginTop: 24,
+  },
 
-  incidentValue: {},
+  incidentValue: {
+    marginTop: 8,
+    fontSize: 15,
+    color: '#737380',
+  },
 
-  contactBox: {},
+  contactBox: {
+    padding: 24,
+    borderRadius: 8,
+    backgroundColor: '#FFF',
+    marginBottom: 16,
+  },
 
-  heroTitle: {},
+  heroTitle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#13131a',
+    lineHeight: 30,
+  },
 
-  heroDescription: {},
+  heroDescription: {
+    fontSize: 15,
+    color: '#737380',
+    marginTop: 16,
+  },
 
-  actions: {},
+  actions: {
+    marginTop: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
 
-  action: {},
+  action: {
+    backgroundColor: '#e02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
-  actionText: {},
+  actionText: {
+    color: '#FFF',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
   
 });
