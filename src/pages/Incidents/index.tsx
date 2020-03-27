260cec74008c5b9b4ea4adffdc771558498d4a79
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 19:12:34 -0300

    Estrutura inicial Incidents

--- src/pages/Incidents/index.tsx
+++ src/pages/Incidents/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
 
 export default function Incidents() {
   return (
-    <View >
-      <Text>
-        Hello caso
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImg} />
+        <Text>
+          Total de <Text style={styles.headerTextBold}>100 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo</Text>
+      <Text style={styles.description}>Escolha um dos casos e salve o dia.</Text>
     </View>
   )
 }
