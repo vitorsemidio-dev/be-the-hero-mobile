2e3cb030d578ea1a1ae7e5f17e7c2581ee4c48d6
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 19:14:58 -0300

    Estilização container

--- src/pages/Incidents/styles.tsx
+++ src/pages/Incidents/styles.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
   header: {},
   headerTextBold: {},
   title: {},
