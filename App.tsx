6b071bf797afc870fb4939625c9539c2bdf57765
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 18:21:20 -0300

    Remoção código e bgColor inicialização app

--- App.tsx
+++ App.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#FFF',
-    fontWeight: 'bold',
-  }
-});
