b1afbc6ebbad0033129b9e1664cdffc71d59c6ce
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-03-27 22:46:42 -0300

    Carregando dados com scroll infinito

--- src/pages/Incidents/index.tsx
+++ src/pages/Incidents/index.tsx
@@ -14,14 +14,33 @@ export default function Incidents() {
   const [incidents, setIncidents] = useState([]);
   const [total, setTotal] = useState(0);
 
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const navigateToDetail = (incident) => {
     navigation.navigate('Detail', { incident });
   }
 
   const loadIncidents = async () => {
-    const response = await api.get('incidents');
-    setIncidents(response.data);
+    if (loading) {
+      return;
+    }
+
+    if (total > 0 && incidents.length === total) {
+      return;
+    }
+
+    setLoading(true);
+
+    const response = await api.get('incidents', {
+      params: { page }
+    });
+
+    
+    setIncidents([...incidents, ...response.data]);
     setTotal(response.headers['x-total-count']);
+    setPage(page + 1);
+    setLoading(false);
   }
 
   useEffect(() => {
@@ -44,7 +63,9 @@ export default function Incidents() {
         style={styles.incidentList}
         data={incidents}
         keyExtractor={incident => String(incident.id)}
-        showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={true}
+        onEndReached={loadIncidents}
+        onEndReachedThreshold={0.8}
         renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
